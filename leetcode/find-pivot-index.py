ef954e7e65e9788b6c71e07a31e06e3c1e2bf5a5
Yididya <jedidiahamdualem@gmail.com>
2023-01-27 21:01:34 +0300

    find pivot index

---
+++ leetcode/find-pivot-index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i-1]
+        if nums[0] == nums[-1]:
+            return 0
+        for i in range(1, len(nums)):
+            if nums[-1] - nums[i] == nums[i-1]:
+                return i
+        return -1
