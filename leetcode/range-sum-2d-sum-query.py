7e5e92f2f5c5f9a4fe7a440b09246ed857d5b7b1
Yididya <jedidiahamdualem@gmail.com>
2023-02-05 20:48:57 +0300

    range-sum-2d-sum-array

--- leetcode/range-sum-2d-sum-query.py
+++ leetcode/range-sum-2d-sum-query.py
@@ -1,14 +1,33 @@
 class NumMatrix:
 
-    def __init__(self, matrix):
-        self.matrix=[i for i in matrix]
-    def sumRegion(self, row1, col1, row2, col2):
-      for i in self.matrix:
-        print(i)
-      print(self.matrix[2][1],self.matrix[4][3])
+    def __init__(self, matrix: List[List[int]]):
+        self.matrix=matrix
+        matrix_length=len(matrix)
+        ith_length=len(matrix[0])
+        self.prefix_sum=[[0 for i in range(ith_length)] for i in range(matrix_length)]
+        self.prefix_sum=[[0 for i in range(len(matrix[0])+1)] for j in range(len(matrix)+1)]
+        for i in range(1,len(matrix[0])+1):
+            self.prefix_sum[1][i]+=self.prefix_sum[1][i-1]+matrix[0][i-1]
+        for j in range(2,len(matrix)+1):
+            self.prefix_sum[j][1]+=self.prefix_sum[j-1][1]+matrix[j-1][0]
+        for row in range(2,len(matrix)+1):
+            for column in range(2,len(matrix[0])+1):
+                self.prefix_sum[row][column]=self.prefix_sum[row-1][column]+self.prefix_sum[row][column-1]-self.prefix_sum[row-1][column-1]+matrix[row-1][column-1]
+        
+        for i in self.prefix_sum:
+            print(i)
+
+        
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        # su=0
+        # for i in range(row1,row2+1):
+        #     su+=sum(self.matrix[i][col1:col2+1])
+        print(self.prefix_sum[row2+1][col2+1],self.prefix_sum[row1][col2+1],self.prefix_sum[row2+1][col1],self.prefix_sum[row1][col1])
+        
+        su=self.prefix_sum[row2+1][col2+1]-self.prefix_sum[row1][col2+1]-self.prefix_sum[row2+1][col1]+self.prefix_sum[row1][col1]
+        return su
 
 
 # Your NumMatrix object will be instantiated and called as such:
-obj = NumMatrix([[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]])
-# [2,1,4,3],[1,1,2,2],[1,2,2,4]
-param_1 = obj.sumRegion(2,1,4,3)
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
