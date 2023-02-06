e61b43c5e907d407691c80c5fa6a6d9bd20666f7
Yididya <jedidiahamdualem@gmail.com>
2023-02-06 10:52:56 +0300

    find all anagrams

--- leetcode/find-all-anagrams-in-a-string.py
+++ leetcode/find-all-anagrams-in-a-string.py
@@ -1,20 +1,20 @@
 class Solution:
-    def findAnagrams(self, s: str, p: str) -> List[int]:
-        count=0
-        k=len(p)
-        curr=s[:k]
-        subar=[]
-        ans=[]
-        p=sorted(p)
-        if sorted(curr)==p:
-            subar.append(curr)
-            ans.append(0)
-        for i in range(1,len(s)-k+1):
-            curr=curr[1:]#-s[i-1]
-            curr=curr+s[i+k-1]
-            # print(curr,set(curr),set(p))
-            if sorted(curr)==p:
-                subar.append(curr)
-                ans.append(i)
-        # print(subar)
-        return ans
+  def findAnagrams(self, s: str, p: str) -> List[int]:
+    count=0
+    k=len(p)
+    curr=s[:k]
+    subar=[]
+    ans=[]
+    p=sorted(p)
+    if sorted(curr)==p:
+      subar.append(curr)
+      ans.append(0)
+    for i in range(1,len(s)-k+1):
+      curr=curr[1:]#-s[i-1]
+      curr=curr+s[i+k-1]
+      # print(curr,set(curr),set(p))
+      if sorted(curr)==p:
+        subar.append(curr)
+        ans.append(i)
+    # print(subar)
+    return ans
